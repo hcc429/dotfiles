fa2ae70ca06ee4274d28bee00b838709c0bd1163
hcc429 <dev.hcc29@gmail.com>
2025-03-10 01:31:45 +0800

    nvim: add copilot to plugin

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -10,7 +10,11 @@ map("i", "jj", "<ESC>")
 map("n", "<leader>d", [["_d]], { desc = "delete without yanking" })
 
 map("n", "<CR>", "o<ESC>", { desc = "add blank line below" })
+
 map("n", "<BS>", "_dd", { desc = "remove a line", noremap = true, silent = true })
+
+-- delete a word with backspace in insert mode
+map("i", "<C-H>", "<C-W>", { desc = "delete word backward", noremap = true, silent = true })
 
 map("n", "<C-d>", "<C-d>zz", { desc = "scroll down and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "scroll up and center" })
@@ -19,3 +23,18 @@ map("n", "N", "Nzzzv", { desc = "search prev and center" })
 
 map("n", "<Tab>", ":bnext<CR>", { desc = "next tab" })
 map("n", "<S-Tab>", ":bprev<CR>", { desc = "previous tab" })
+
+map("n", "L", "$")
+map("n", "H", "^")
+
+-- toggle copilot
+local copilot_enabled = true
+map("n", "<leader>ct", function()
+  if copilot_enabled then
+    vim.cmd("Copilot disable")
+    copilot_enabled = false
+  else
+    vim.cmd("Copilot enable")
+    copilot_enabled = true
+  end
+end, { desc = "toggle copilot" })

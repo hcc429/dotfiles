e9571b5d99fc368534a44038f7f0a5652d68c38b
thomascheng <thomascheng@11401-DT-009.local>
2025-03-10 18:41:37 +0800

    nvim: add <C-H> keymap in visual mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,6 +12,7 @@ map("n", "<leader>d", [["_d]], { desc = "delete without yanking" })
 map("n", "<CR>", "o<ESC>", { desc = "add blank line below" })
 
 map("n", "<BS>", "_dd", { desc = "remove a line", noremap = true, silent = true })
+map("x", "<C-H>", "_d", { desc = "remove selected text", noremap = true, silent = true })
 
 -- delete a word with backspace in insert mode
 map("i", "<C-H>", "<C-W>", { desc = "delete word backward", noremap = true, silent = true })

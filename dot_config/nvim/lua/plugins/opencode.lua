92f31b43a4a4fabb9bf97b2520563c8b97e9178f
cc <dev.hcc29@gmail.com>
2026-01-04 16:19:44 +0000

    opencode use tmux provider

--- dot_config/nvim/lua/plugins/opencode.lua
+++ dot_config/nvim/lua/plugins/opencode.lua
@@ -10,6 +10,10 @@ return {
     ---@type opencode.Opts
     vim.g.opencode_opts = {
       -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      provider = {
+        enabled = 'tmux',
+        tmux = {},
+      },
     }
 
     -- Required for `opts.events.reload`.
@@ -19,9 +23,9 @@ return {
     vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
       require('opencode').ask('@this: ', { submit = true })
     end, { desc = 'Ask opencode' })
-    vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
-      require('opencode').select()
-    end, { desc = 'Execute opencode action…' })
+    vim.keymap.set({ 'n', 'x' }, '<C-.>', function()
+      require('opencode').toggle()
+    end, { desc = 'Toggle opencode' })
 
     -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
     vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment', noremap = true })
